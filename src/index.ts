a1a315183787066ee93d95b32c89f0f39045f1ae
srinibas khuntia <srinibas.k@pillir.io>
2025-04-22 01:11:55 +0530

    created skeleton endpoints

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,42 @@ const port = 5000;
 
 app.use(express.json())
 
+
+app.post("/api/v1/signup",(req,res)=>{
+
+})
+
+app.post("/api/v1/signin",(req,res)=>{
+
+})
+
+//creating content
+app.post("api/v1/content",(req,res)=>{
+
+
+})
+
+//getting content
+app.get("api/v1/content",(req,res)=>{
+
+})
+
+//deleting content
+app.delete("api/v1/delete/:id",(req,res)=>{
+
+})
+
+
+//creating a sharable link
+app.post("api/v1/brain/share",(req,res)=>{
+
+})
+//fetching the shared link
+app.get("api/v1/brain/share/:shareId",(req,res)=>{
+
+})
+
+
 app.get("/",(req,res)=>{
     res.json({
         message:"your app has been started"
